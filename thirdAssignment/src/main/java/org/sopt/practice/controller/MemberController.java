e4951799c22486528f0d9cd60c1c9eb7aac4a331
sansan20535 <20san20@daum.net>
2024-04-29 01:07:14 +0900

    start thirdAssignment

---
+++ thirdAssignment/src/main/java/org/sopt/practice/controller/MemberController.java
@@ -0,0 +1,36 @@
+package org.sopt.practice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.practice.service.MemberService;
+import org.sopt.practice.dto.MemberCreateDto;
+import org.sopt.practice.dto.MemberFindDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity createMember(
+            @RequestBody MemberCreateDto memberCreateDto
+    ){
+        return ResponseEntity.created(URI.create(memberService.createMember(memberCreateDto))).build();
+    }
+
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberFindDto> findMemberById(@PathVariable("memberId") Long memberId){
+        return ResponseEntity.ok(memberService.findMemberById(memberId));
+    }
+
+    @DeleteMapping("/{memberId}")
+    public ResponseEntity deleteMemberById(@PathVariable("memberId") Long memberId){
+        memberService.deleteMemberById(memberId);
+        return ResponseEntity.noContent().build();
+    }
+}
